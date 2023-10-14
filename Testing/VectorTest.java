6117dd19f9ec969633e08206dedeb6eab6c04300
septicake <septicake056@gmail.com>
2023-10-14 10:53:39 -0400

    Removed comments

--- Testing/VectorTest.java
+++ Testing/VectorTest.java
@@ -4,18 +4,13 @@ import Bases.VectorBase;
 import Vectors.*;
 import Tools.*;
 
-//TODO: Cry over thread interrupts because they're mean and I don't like them
 public class VectorTest {
     public static void main(String[] args) {
         VectorBase v = VectorBase.of(7, 14, 45, 6);
         System.out.println(v);
         System.out.println(v + " magnitude: " + v.getMagnitude());
         v.setMax(1);
-        /*
-         * I'm gonna commit SO MANY CRIMES
-         * The gods will fear my wrath cause this is some real bullshit
-         */
-        System.out.println("CHECK: " + v + " magnitude: " + v.getMagnitude()); //Thread interrupts are mean >:(
+        System.out.println("CHECK: " + v + " magnitude: " + v.getMagnitude());
         System.out.println("RERUN: magnitude: " + Math.sqrt(OpVectors.squareSum(v)));
         v.setMax(0);
         System.out.println(v.set(0, 0));
@@ -49,6 +44,7 @@ public class VectorTest {
         System.out.println("Unit Output: " + unit);
         System.out.println("Unit Magnitude: " + unit.getMagnitude());
 
+        System.out.println("\nIteration:");
         VectorBase forEach = VectorBase.of(1, 4, 6, 8, 10);
         for(double d : forEach){
             System.out.println(d);
